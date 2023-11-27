93c676e538fb501d7df4c6996914118050da473d
ElizabethChidinma <echidinma68@gmail.com>
2023-11-27 13:36:21 -0800

    update

---
+++ 0x00-python-hello_world/lists.h
@@ -0,0 +1,16 @@
+#include "lists.h"
+#include <stdio.h>
+
+int check_cycle(listint_t *list)
+{
+	listint_t *fast + list, *slow = list;
+
+	while (fast != NULL && slow != NULL && fast->next != NULL)
+	{
+		slow = slow->next;
+		fast = fast->next->next;
+		if (fast == slow)
+			return (1);
+	}
+	return (0);
+}
