50a3306a1df9fff31d45b96d8ddacacf6318319e
ElizabethChidinma <echidinma68@gmail.com>
2023-11-30 10:22:51 -0800

    date

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-from add_0 import add
 if __name__ == "__main__":
-    a = 1
+    from add_0 import add
+      a = 1
     b = 2
-    c = add(a, b)
-    print(f"{a} + {b} = {c}")
+    print("{} + {} = {}".format(a, b, add(a, b)))
